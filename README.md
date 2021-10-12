b92d9ed14ef6bda70b52ce1365b5dace4e4954ae
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-10-12 22:35:48 +0200

    Add layout and request to IMDB - still doesnt work

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# movieTracker
+# Movie Tracker
 Mobile app for tracking watched movies and series.
--- app/src/main/java/com/movietracker/MainActivity.kt
+++ app/src/main/java/com/movietracker/MainActivity.kt
@@ -1,4 +1,31 @@
 package com.movietracker
 
-class MainActivity {
+import android.os.Bundle
+import android.os.StrictMode
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20throne")
+            .get()
+            .addHeader("x-rapidapi-host", "imdb8.p.rapidapi.com")
+            .addHeader("x-rapidapi-key", "YOUR_KEY")
+            .build()
+
+        val response = client.newCall(request).execute()
+
+//        val textView = findViewById<TextView>(R.id.text1)
+//        textView.text = "test 123"
+    }
+
 }
